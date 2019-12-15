7309e85d2e0b369e9fd3b1f0f14553343085cd07
manikandanthangam <manikandantt91@gmail.com>
2019-12-15 13:14:55 +0530

    Initial deployment

---
+++ precache-manifest.c07928feaf96c1609875df5546b775de.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316a02aa4429029360b9eb37a4439585",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc74add169aa8739a4b5",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "9d3f2e2005e08ea67a31",
+    "url": "/static/css/main.aaa29599.chunk.css"
+  },
+  {
+    "revision": "fc74add169aa8739a4b5",
+    "url": "/static/js/2.09181cf1.chunk.js"
+  },
+  {
+    "revision": "9d3f2e2005e08ea67a31",
+    "url": "/static/js/main.536d198b.chunk.js"
+  },
+  {
+    "revision": "5fa28fa4c207d199b759",
+    "url": "/static/js/runtime-main.6a7d632f.js"
+  }
+]);
